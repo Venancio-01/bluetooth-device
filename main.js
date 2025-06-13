9f6076ececbff31df09421b6e7e16c807b670586
vaausud <15054854614@163.com>
2025-06-13 16:45:35 +0800

    1

--- main.js
+++ main.js
@@ -4,6 +4,11 @@ const { ReadlineParser } = require('@serialport/parser-readline');
 const SERIAL_PORT_PATH = '/dev/ttyUSB0';
 const BAUD_RATE = 115200;
 
+const AT_MODE_ENTRY_CMD = '+++'; // 进入 AT 模式的命令，不需要回车换行符
+const AT_MODE_ENTRY_RESPONSE = 'OK'; // 进入 AT 模式后模块返回的响应
+const QUERY_DEVICE_NAME_CMD = 'AT+NAME?'; // 查询设备名称的 AT 指令
+const AT_COMMAND_SUFFIX = '\r\n'; // 所有 AT 命令都必须以回车换行符结尾
+
 let port;
 
 function openSerialPort() {
@@ -13,6 +18,7 @@ function openSerialPort() {
     dataBits: 8,
     stopBits: 1,
     parity: 'none',
+    autoOpen: false,
   }, (err) => {
     if (err) {
       return console.error('打开串口时出错:', err.message);
@@ -21,6 +27,19 @@ function openSerialPort() {
   });
 
   const parser = port.pipe(new ReadlineParser({ delimiter: '\r\n' }));
+
+  // 串口打开事件
+  port.on('open', () => {
+    console.log('串口已成功打开。');
+
+    // 模块上电后需要 600ms 才能正常工作
+    // 建议等待至少 1 秒，以确保模块稳定并准备好接收命令。
+    setTimeout(() => {
+      console.log(`发送 "${AT_MODE_ENTRY_CMD}" 尝试进入 AT 指令模式...`);
+      // 发送进入 AT 模式的命令，不带回车换行符
+      sendData(AT_MODE_ENTRY_CMD, false);
+    }, 1000);
+  });
 
   // 监听数据接收事件
   parser.on('data', data => {
@@ -37,11 +56,14 @@ function openSerialPort() {
   port.on('close', () => {
     console.log('串口已关闭');
   });
+
+  // 打开串口
+  port.open();
 }
 
-function sendData(data) {
+function sendData(data, newline = true) {
   if (port && port.isOpen) {
-    port.write(data + '\n', (err) => {
+    port.write(data + (newline ? AT_COMMAND_SUFFIX : ''), (err) => {
       if (err) {
         return console.error('发送数据时出错:', err.message);
       }
@@ -68,7 +90,7 @@ function closeSerialPort() {
 openSerialPort();
 
 setTimeout(() => {
-  sendData('AT+NAME?');
+  sendData(QUERY_DEVICE_NAME_CMD, true);
 }, 2000);
 
 process.on('SIGINT', () => {
