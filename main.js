c61cebeaec23ca25955bbde576c7bd2d30c12ddf
vaausud <15054854614@163.com>
2025-06-13 16:04:32 +0800

    1

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
 const usb = require('usb');
 
+const vid = 0x10c4;
+const pid = 0xea60;
+
 function main(){
   const devices = usb.getDeviceList();
   console.log(devices);
+
+  const device = devices.find(d => d.deviceDescriptor.idVendor === vid && d.deviceDescriptor.idProduct === pid);
+  console.log(device);
+
+  if(device){
+    console.log('Device found');
+  }
 }
 
 
