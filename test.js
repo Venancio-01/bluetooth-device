ea91309654c9907c1d44adb2650cf25e9333101e
vaausud <15054854614@163.com>
2025-06-18 13:36:49 +0800

    1

---
+++ test.js
@@ -0,0 +1,30 @@
+const process = require('process')
+const { SerialPort } = require('serialport')
+
+let port = null
+
+function main() {
+  port = new SerialPort({
+    path: '/dev/ttyS3',
+    baudRate: 115200,
+    dataBits: 8,
+    stopBits: 1,
+    parity: 'none',
+    timeout: 5000,
+  })
+
+  port.on('open', () => {
+    console.log('open')
+  })
+
+  port.on('data', (data) => {
+    console.log('data', data)
+  })
+}
+
+main()
+
+process.on('SIGINT', () => {
+  port.close()
+  process.exit(0)
+})
